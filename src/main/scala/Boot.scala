4d744860efc1cbc6a43add59a5d49ebf08037169
huydx <doxuanhuy@gmail.com>
2015-12-12 21:42:05 +0900

    string/string key value

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,4 +1,5 @@
 object Boot extends App {
-    val node = new Node()
-    node.putIfAbscent("foo", 2L)
+    val node = new Storage(100L)
+    node.putIfAbscent("foo", "sadasdad asdas dasd asd asda sdasda sda d")
+    node.get("foo")
 }
